cee14061d291aa4a812073bc8d1a04ef375597d3
FerencOnodi <onodiferenc91@gmail.com>
2017-09-20 10:03:25 +0200

    First commit.

---
+++ src/solutions/MySolution.java
@@ -0,0 +1,73 @@
+package solutions;
+
+public class MySolution implements Solution {
+    @Override
+    public String reverseString(String word) {
+        return null;
+    }
+
+    @Override
+    public long factorialize(int number) {
+        return 0;
+    }
+
+    @Override
+    public boolean palindrome(String word) {
+        return false;
+    }
+
+    @Override
+    public int findLongestWord(String word) {
+        return 0;
+    }
+
+    @Override
+    public String titleCase(String word) {
+        return null;
+    }
+
+    @Override
+    public int[] largestOfFour(int[][] numbers) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean confirmEnding(String word, String end) {
+        return false;
+    }
+
+    @Override
+    public String repeatStringNumTimes(String word, int counter) {
+        return null;
+    }
+
+    @Override
+    public String truncateString(String word, int counter) {
+        return null;
+    }
+
+    @Override
+    public Object[][] chunkArrayInGroups(Object[] numbers, int counter) {
+        return new Object[0][];
+    }
+
+    @Override
+    public Object[] slasher(Object[] numbers, int counter) {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] destroyer(Object[] numbers, Object... remove) {
+        return new Object[0];
+    }
+
+    @Override
+    public int getIndexToIns(Object[] numbers, float number) {
+        return 0;
+    }
+
+    @Override
+    public String rot13(String word) {
+        return null;
+    }
+}
